5d2ee37e7ac3a92a6d082aa04ab5c306a027c855
Hannah Cacapit <hcacapit.sh@mysummitps.org>
2016-11-09 19:59:21 +0000

    update

--- myGame/story.js
+++ myGame/story.js
@@ -13,7 +13,7 @@ game_state.story.prototype = {
     create: function(){
         game.add.sprite(0, 0, 'sky');
 
-        game.add.sprite(450, 460, 'star');
+        game.add.sprite(570, 20, 'star');
 
         this.platforms = game.add.group();
         var ground = this.platforms.create(0, game.world.height - 64, 'ground');
@@ -21,7 +21,7 @@ game_state.story.prototype = {
         
         game.add.sprite(300, 433, 'character');
 
-        this.storyText = game.add.text(20, 50, ' Greetings from the collector of dreams! \r When people make wises, those fall from the skies \r in the form of what you people call shooting stars. \r My job is to collect those dreams \r or they fade and will not come true. \r I can only carry at 100 dreams at a time',{
+        this.storyText = game.add.text(20, 50, ' Greetings from the collector of dreams! \r When people make wishes, those fall from the skies \r in the form of what you people call shooting stars. \r I have the power to make those dreams come true \r and so it is my job to collect them. \r \r I can only carry at 100 dreams at a time',{
             fontSize: '32px',
             fill: '#000' 
 
